c10b75ee27ea2eb8b0dda64d90cfe1b120decfc7
ef10007 <jinseon-han@hotmail.com>
2018-11-14 17:26:12 +0900

    first commit

---
+++ portfolio/blackjacksjinny.py
@@ -0,0 +1,100 @@
+class Card:
+    def __init__(self):
+        self.name = "카드"
+
+    def card(self):
+        import random
+        s = ["Spade", "Diamond", "Heart", "Clover"]
+        n = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        card = []
+        i = random.choice(s)
+        j = random.choice(n)
+        a = i + j
+        card.append(a)
+        print(card)
+        
+    def sum(self, x, y):
+        b = x + y
+        return sum(b)
+
+
+카드 = Card()
+카드.card()
+
+
+# class Person:
+#     def __init__(self):
+#         self.name = "사용자"
+#     def sum(self, x, y):
+#         return x + y
+
+#     class Dealer(Person):
+#         name = "딜러"
+        
+#     class Player(Person):
+#         name = "플레이어"
+
+
+    # class Deck(Card):
+    #     def __init__(self, s, n):
+    #         self.s = "모양"
+    #         self.n = "숫자"
+
+#     hos = input("Hit or stay? >>>")
+
+#     def deck(self):
+#         if card == hit:
+#             card.join(list(card))
+
+
+# class Person:
+#     def __init__(self):
+#         self.name = "사용자"
+#     def sum(self, x, y):
+#         return sum += x + y
+
+
+# class Dealer(Person):
+#     name = "딜러"
+
+# class Player(Person):
+#     name = "플레이어"
+
+# 딜러 = Dealer
+# 플레이어 = Player
+
+
+#######################################
+
+# from functools import reduce
+# product = 1
+# lst = [1, 2, 3, 4]
+# for num in lst:
+#     product = product * num
+
+# print("product1>>", product)
+
+# product2 = reduce(lambda x, y: x * y, lst)
+# print("product2>>", product2)
+
+
+######################################3
+
+
+# for x, y in list:
+
+# if sum == 21:
+#     print("Jackpot!!")
+#     break
+
+# if sum > 21:
+#     print("Game over")
+#     break
+
+# else:
+#     while True:
+#         print("Compare each sum")
+#         if sum.Dealer > sum.Player:
+#             print("Dealer win!")
+#         else:
+#             print("Player win!")
